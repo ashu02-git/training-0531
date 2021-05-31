edeba3dcf94ce6e2965e2d9af7c55948add7766b
ashu02-git <shuhei.abe1107@gmail.com>
2021-05-31 13:18:36 +0900

    URL Moduleでルーティング

--- app.js
+++ app.js
@@ -1,16 +1,59 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
+const moment = require('moment');
+const url = require('url');
 
-const index = fs.readFileSync('./index.ejs','utf8')
+moment.locale('js');
+let datetoday = moment().format('LL');
+
+const index = fs.readFileSync('./index.ejs','utf8');
+const sample = fs.readFileSync('./sample.ejs','utf8');
+const style_css = fs.readFileSync('./style.css','utf8');
+
 
 let server = http.createServer(getFromCliant);
 server.listen(3000);
 console.log('Server start!')
+console.log(datetoday);
 
 function getFromCliant(req,res){
-    let content = ejs.render(index);
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.write(content);
-    res.end();
+
+    let url_parts = url.parse(req.url);
+    console.log(url_parts);
+
+    switch(url_parts.pathname){
+
+        case '/':
+            var content = ejs.render(index,{
+                title: 'Kikuchannel',
+                today:datetoday,
+            }); 
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.write(content);
+            res.end();
+            break;
+
+        case '/sample':
+            var content = ejs.render(sample,{
+                title: 'introduction',
+                today:datetoday,
+            }); 
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.write(content);
+            res.end();
+            break;
+
+        case '/style.css':
+            res.writeHead(200, {'Content-Type':'text/css'});
+            res.write(style_css);
+            res.end();
+            break;
+
+        default:
+            res.writeHead(404, {'Content-Type':'text/plain'});
+            res.end('no page...');
+            break;
+
+    }
 }
